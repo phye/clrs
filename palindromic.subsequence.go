70c6e53937dfe817265c4df69beb7ef87ddc6ec1
Philip Ye <phye@cisco.com>
2017-11-10 14:56:11 +0800

    Add palindromic subsequence solution

---
+++ palindromic.subsequence.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func LongestPalindromicSubsequence(A string) int {
+	L := make([][]int, len(A))
+	for i := 0; i < len(A); i++ {
+		L[i] = make([]int, len(A))
+	}
+	longestPalindromicSubsequence(A, 0, len(A)-1, L)
+	return L[0][len(A)-1]
+}
+
+func longestPalindromicSubsequence(A string, start int, end int, L [][]int) {
+	if start > end {
+		return
+	}
+	if start == end {
+		L[start][end] = 1
+		return
+	}
+	if A[start] == A[end] {
+		longestPalindromicSubsequence(A, start+1, end-1, L)
+		L[start][end] = L[start+1][end-1] + 2
+	} else {
+		longestPalindromicSubsequence(A, start+1, end, L)
+		longestPalindromicSubsequence(A, start, end-1, L)
+		if L[start+1][end] > L[start][end-1] {
+			L[start][end] = L[start+1][end]
+		} else {
+			L[start][end] = L[start][end-1]
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("B"))
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("BB"))
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("BBA"))
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("BAB"))
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("BBABCBCAB"))
+}
