e3208fbe4c8621b2e9b3239d7d398c9e17d272f2
Philip Ye <phye@cisco.com>
2017-11-10 15:53:05 +0800

    Bug fix for palindromic subsequence

--- palindromic.subsequence.go
+++ palindromic.subsequence.go
@@ -10,11 +10,14 @@ func LongestPalindromicSubsequence(A string) int {
 		L[i] = make([]int, len(A))
 	}
 	longestPalindromicSubsequence(A, 0, len(A)-1, L)
+	for i := 0; i < len(A); i++ {
+		fmt.Printf("%v\n", L[i])
+	}
 	return L[0][len(A)-1]
 }
 
 func longestPalindromicSubsequence(A string, start int, end int, L [][]int) {
-	if start > end {
+	if start > end || L[start][end] > 0 {
 		return
 	}
 	if start == end {
@@ -41,5 +44,5 @@ func main() {
 	fmt.Printf("%v\n", LongestPalindromicSubsequence("BB"))
 	fmt.Printf("%v\n", LongestPalindromicSubsequence("BBA"))
 	fmt.Printf("%v\n", LongestPalindromicSubsequence("BAB"))
-	fmt.Printf("%v\n", LongestPalindromicSubsequence("BBABCBCAB"))
+	fmt.Printf("%v\n", LongestPalindromicSubsequence("BBBABCBCABBABCBCAB"))
 }
