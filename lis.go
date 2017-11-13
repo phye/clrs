6b8e0f367b0dd156516ad16c9432a12f54aa508b
Philip Ye <phye@cisco.com>
2017-11-13 14:08:05 +0800

    Add more compact LIS solution

--- lis.go
+++ lis.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 )
 
-func LIS(A []int) int {
+func LIS0(A []int) int {
 	lis := make(map[int]int, 0)
-	_LIS(A, len(A)-1, lis)
+	_LIS0(A, len(A)-1, lis)
 	max := -1
 	for i := 0; i < len(A); i++ {
 		if lis[i] > max {
@@ -18,13 +18,13 @@ func LIS(A []int) int {
 	return max
 }
 
-func _LIS(A []int, i int, lis map[int]int) {
+func _LIS0(A []int, i int, lis map[int]int) {
 	if _, ok := lis[i]; ok {
 		return
 	}
 	max := -1
 	for j := i - 1; j >= 0; j-- {
-		_LIS(A, j, lis)
+		_LIS0(A, j, lis)
 		if A[j] < A[i] && lis[j] > max {
 			max = lis[j]
 		}
@@ -37,7 +37,33 @@ func _LIS(A []int, i int, lis map[int]int) {
 	return
 }
 
+func LIS(A []int) int {
+	max := 1
+	_LIS(A, len(A)-1, &max)
+	return max
+}
+
+// Return: length of increasing subsequence at index n
+// max: Length of LIS in A
+func _LIS(A []int, i int, pm *int) int {
+	if i == 0 {
+		return 1
+	}
+	ret := 1
+	for j := 0; j < i; j++ {
+		oret := _LIS(A, j, pm)
+		if A[j] < A[i] && oret+1 > ret {
+			ret = oret + 1
+		}
+	}
+	if ret > *pm {
+		*pm = ret
+	}
+	return ret
+}
+
 func main() {
 	A := []int{10, 22, 9, 33, 21, 50, 41, 60, 80}
+	fmt.Printf("%v\n", LIS0(A))
 	fmt.Printf("%v\n", LIS(A))
 }
