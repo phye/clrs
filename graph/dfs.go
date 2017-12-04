19a6f281489f7c69723eb72b36f0eb4c24e946e8
Philip Ye <phye@cisco.com>
2017-12-04 17:31:05 +0800

    Add dfs for graph

---
+++ graph/dfs.go
@@ -0,0 +1,63 @@
+package graph
+
+import (
+	"fmt"
+)
+
+type (
+	DFSAux struct {
+		color int
+		pi    *Node
+		d     int // Time at which the node is discovered
+		f     int // Time at which the node is fully explored
+	}
+	DFSStatus map[*Node]*DFSAux
+)
+
+var (
+	time int = 0
+)
+
+func (g *Graph) DFS() DFSStatus {
+	dfsStatus := make(DFSStatus, 0)
+	for _, n := range g.nodes {
+		dfsStatus[n] = &DFSAux{WHITE, nil, INF, INF}
+	}
+	for _, n := range g.nodes {
+		if dfsStatus[n].color == WHITE {
+			g.dfsVisit(n, dfsStatus)
+		}
+	}
+	fmt.Printf("%s", dfsStatus)
+	return dfsStatus
+}
+
+func (g *Graph) dfsVisit(u *Node, dfsStatus DFSStatus) {
+	time++
+	dfsStatus[u].d = time
+	dfsStatus[u].color = GRAY
+	for _, v := range g.Adj(u) {
+		if dfsStatus[v].color == WHITE {
+			dfsStatus[v].pi = u
+			g.dfsVisit(v, dfsStatus)
+		}
+	}
+	dfsStatus[u].color = BLACK
+	time++
+	dfsStatus[u].f = time
+}
+
+func (dfss DFSStatus) String() string {
+	var ret string
+	ret += fmt.Sprintf("------------DFS------------\n")
+	ret += fmt.Sprintf("%8s %8s %8s %8s\n", "current", "parent", "start", "finish")
+	for n, s := range dfss {
+		if s.pi != nil {
+			ret += fmt.Sprintf("%8v %8v %8v %8v\n", n.value, s.pi.value, s.d, s.f)
+		} else {
+			ret += fmt.Sprintf("%8v %8v %8v %8v\n", n.value, "nil", s.d, s.f)
+		}
+	}
+	ret += fmt.Sprintf("---------------------------\n")
+	return ret
+}
---
+++ graph/dfs_test.go
@@ -0,0 +1,30 @@
+package graph
+
+import (
+	"testing"
+)
+
+func TestDFS(test *testing.T) {
+	g := NewGraph(false)
+	r := g.AddNode("r")
+	s := g.AddNode("s")
+	t := g.AddNode("t")
+	u := g.AddNode("u")
+	v := g.AddNode("v")
+	w := g.AddNode("w")
+	x := g.AddNode("x")
+	y := g.AddNode("y")
+
+	g.AddEdge(r, v, 1)
+	g.AddEdge(r, s, 1)
+	g.AddEdge(s, w, 1)
+	g.AddEdge(w, t, 1)
+	g.AddEdge(w, x, 1)
+	g.AddEdge(t, x, 1)
+	g.AddEdge(t, u, 1)
+	g.AddEdge(x, u, 1)
+	g.AddEdge(x, y, 1)
+	g.AddEdge(u, y, 1)
+
+	g.DFS()
+}
