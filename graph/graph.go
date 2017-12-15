27c46249278f7da61400125d3febaaf0f7ab8700
Philip Ye <phye@cisco.com>
2017-12-15 15:00:55 +0800

    Add more comments for Graph interface

--- graph/graph.go
+++ graph/graph.go
@@ -6,10 +6,15 @@ import (
 
 type (
 	Graph interface {
+		// Add node v into graph, interface{} MUST be comparable! (thus cannot be slice)
 		AddNode(v interface{})
+		// Add edge (sv, ev) into graph, if graph is not directed, two edges will be added
 		AddEdge(sv, ev interface{}, weight int) error
+		// Return a slice of all nodes' keys in graph
 		Nodes() []interface{}
+		// Return adjacent nodes' keys of node v
 		Adj(v interface{}) ([]interface{}, error)
+		// Return weight of edge (sv, ev)
 		Weight(sv, ev interface{}) (int, error)
 
 		fmt.Stringer
