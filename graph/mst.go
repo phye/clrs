1ff57b675d109360f54a7a50e5cc497b84969dba
Philip Ye <phye@cisco.com>
2017-12-18 14:30:17 +0800

    Align MST-PRIM to new graph API

--- graph/mst.go
+++ graph/mst.go
@@ -7,23 +7,20 @@ import (
 
 type (
 	PRIMAux struct {
-		c        *Node // Current node, reverse pointer for quicker lookup
-		pi       *Node // Parent node, filled during PRIM MST execution
-		isolated bool  // Is the node still in Q?
+		isolated bool // Is the node still in Q?
+		NodeAux
 	}
-	PRIMStatus map[*Node]*PRIMAux
+	PRIMStatus map[interface{}]*PRIMAux
 )
 
 // Return an MST graph constructed via PRIM algorithm
-func (g *Graph) PrimMST(r *Node) *Graph {
-	ng := NewGraph(g.directed)
-	// Temp map to store g.node <-> ng.node
-	nm := make(map[*Node]*Node)
+func PrimMST(g Graph, r interface{}) Graph {
+	ng := NewGraph(g.Directed(), g.GraphType())
 
 	psm := make(PRIMStatus)
 	Q := ds.NewMinPriorityQueue()
-	for _, n := range g.nodes {
-		psm[n] = &PRIMAux{n, nil, true}
+	for _, n := range g.Nodes() {
+		psm[n] = &PRIMAux{true, NodeAux{n, nil, 0}}
 		// NOTE: ds.Item.Value is a pointer to PRIMAux
 		Q.Push(psm[n], INF)
 	}
@@ -36,15 +33,17 @@ func (g *Graph) PrimMST(r *Node) *Graph {
 		pma := uitem.Value.(*PRIMAux) // Value is interface {} which is *PRIMAux in this case
 		pma.isolated = false          // Once a node is POPed from Q, it is no longer isolated
 		u := pma.c
-		nm[u] = ng.AddNode(u.value)
+		ng.AddNode(u)
 		if psm[u].pi != nil {
 			pi := psm[u].pi
-			ng.AddEdge(nm[pi], nm[u], g.Edge(pi, u).weight)
+			wt, _ := g.Weight(pi, u)
+			ng.AddEdge(pi, u, wt)
 		}
-		for _, v := range g.Adj(u) {
+		adjs, _ := g.Adj(u)
+		for _, v := range adjs {
 			// if v is in Q
 			if psm[v].isolated {
-				weight := g.Edge(u, v).weight
+				weight, _ := g.Weight(u, v)
 				vitem := Q.Item(psm[v])
 				if weight < vitem.Priority {
 					psm[v].pi = u
@@ -61,9 +60,9 @@ func (ps PRIMStatus) String() string {
 	ret += fmt.Sprintf("%8s %8s\n", "current", "parent")
 	for _, n := range ps {
 		if n.pi != nil {
-			ret += fmt.Sprintf("%8s %8s\n", n.c.value, n.pi.value)
+			ret += fmt.Sprintf("%8s %8s\n", n.c, n.pi)
 		} else {
-			ret += fmt.Sprintf("%8s %8s\n", n.c.value, "NIL")
+			ret += fmt.Sprintf("%8s %8s\n", n.c, "NIL")
 		}
 	}
 	return ret
